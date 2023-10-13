ee4eafe7d97872c426324330b4a627bd8af6209e
kram <i2395787@icloud.com>
2023-10-13 12:24:42 +0400

    fix bug 2

--- backtotop.js
+++
@@ -1,5 +0,0 @@
-var s = document.createElement("script");
-	s.type = "text/javascript";
-	s.src = "https://cdn.jsdelivr.net/gh/dmitrykram/Ecwidbacktotop@main/append.js";
-	console.log('вывод')
-	document.head.appendChild(s);
